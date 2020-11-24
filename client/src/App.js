455b432359fc23623dd51a84e685d01ae036cdac
Semy <songyein620@gmail.com>
2020-11-24 20:27:38 +0900

    proxy 설정

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,52 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-    'id':1,
-    'image': 'https://placeimg.com/64/64/any',
-    'name': '홍길동',
-    'birthday': '970603',
-    'gender':'남자',
-    'job':'대학생'
-  },
-  {
-    'id':2,
-    'image': 'https://placeimg.com/64/64/any',
-    'name': '홍길동',
-    'birthday': '970603',
-    'gender':'남자',
-    'job':'대학생'
-  },
-  {
-      'id':3,
-      'image': 'https://placeimg.com/64/64/any',
-      'name': '홍길동',
-      'birthday': '970603',
-      'gender':'남자',
-      'job':'대학생'
-  },
-]
+// const customers = [
+//   {
+//     'id':1,
+//     'image': 'https://placeimg.com/64/64/any',
+//     'name': '홍길동',
+//     'birthday': '970603',
+//     'gender':'남자',
+//     'job':'대학생'
+//   },
+//   {
+//     'id':2,
+//     'image': 'https://placeimg.com/64/64/any',
+//     'name': '홍길동',
+//     'birthday': '970603',
+//     'gender':'남자',
+//     'job':'대학생'
+//   },
+//   {
+//       'id':3,
+//       'image': 'https://placeimg.com/64/64/any',
+//       'name': '홍길동',
+//       'birthday': '970603',
+//       'gender':'남자',
+//       'job':'대학생'
+//   },
+// ]
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  //모든 컴포넌트가 마운트 되었을때 실행
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err=>console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();//
+    return body;
+  }
+
   render(){
     const { classes } = this.props;
     return (
@@ -64,7 +82,8 @@ class App extends Component {
               </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c=> { return  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> }) }
+          {this.state.customers ? this.state.customers.map(c=> { return  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> 
+          }):""}
           </TableBody>
         </Table>
       </Paper>
